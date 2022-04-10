9b4f4945e221009e2dce6f93b896541d8d869b8a
shiyaamsundar <40712528+shiyaamsundar@users.noreply.github.com>
2022-04-10 17:15:20 +0530

    commit

--- server.js
+++ server.js
@@ -139,7 +139,7 @@ app.post("/send", function (req, res) {
   });
 });
 
-const port = 3001;
+const port = Process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
 });
